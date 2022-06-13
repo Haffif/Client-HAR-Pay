d0332bf3b5dfa0dd3bdf77d248c5b5020c603939
Haffif <haffifrasyafauzi@gmail.com>
2022-06-13 16:06:21 +0700

    Feat: cek saldo at dashboard peepay

--- js/dashboard/dashboard.js
+++ js/dashboard/dashboard.js
@@ -13,9 +13,15 @@ if (!edoeit) {
   window.location.href = "../home/home.html";
 }
 
+const peepay = JSON.parse(localStorage.getItem("peepay"));
+if (!peepay) {
+  window.location.href = "../home/home.html";
+}
+
 const saldo = document.getElementById("saldo");
 const saldoMet4kantin = document.getElementById("saldomet4");
 const saldoEdoeit = document.getElementById("saldoedoeit");
+const saldoPeepay = document.getElementById("saldopeepay");
 
 var myHeaders = new Headers();
 myHeaders.append("Authorization", `Bearer ${eclogin.jwt}`);
@@ -29,6 +35,7 @@ var requestOptions = {
 const harpayCekSaldoUrl = "https://harpay-api.herokuapp.com/transaksi/cekSaldo";
 const met4kantinCekSaldoUrl = "https://met4kantin.herokuapp.com/api/profile";
 const edoeitCekSaldoUrl = "https://e-doeit.herokuapp.com/api/profile";
+const peepayCekSaldoUrl = "https://peepaywallet-v2.herokuapp.com/api/profile"; // get
 
 fetch(harpayCekSaldoUrl, requestOptions)
   .then((response) => response.json())
@@ -54,6 +61,18 @@ fetch(harpayCekSaldoUrl, requestOptions)
           .then((responsoe) => responsoe.json())
           .then((result) => {
             saldoEdoeit.innerText = result.saldo;
+
+            // setup new headers
+            var newHeadersPeepay = new Headers();
+            newHeadersPeepay.append("Authorization", `Bearer ${peepay.jwt}`);
+            requestOptions.headers = newHeadersPeepay;
+
+            fetch(peepayCekSaldoUrl, requestOptions)
+              .then(response => response.json())
+              .then(result => {
+                saldoPeepay.innerText = result.balance;
+              })
+              .catch(err => console.log(err))
           })
           .catch((err) => console.log(err));
       })

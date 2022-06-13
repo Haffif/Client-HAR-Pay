38d6244cc8a6b63daf5aea576de1b4afb04dfaf3
Rama Muhammad Murshal <rmurshal@gmail.com>
2022-06-13 11:02:09 +0700

    Feat: finish history transaction edoeit, met4kantin doesnt provide

--- js/dashboard/history.js
+++ js/dashboard/history.js
@@ -1,6 +1,15 @@
 const eclogin = JSON.parse(localStorage.getItem("eclogin"));
-
 if (!eclogin) {
+  window.location.href = "../home/home.html";
+}
+
+const met4kantin = JSON.parse(localStorage.getItem("met4kantin"));
+if (!met4kantin) {
+  window.location.href = "../home/home.html";
+}
+
+const edoeit = JSON.parse(localStorage.getItem("edoeit"));
+if (!edoeit) {
   window.location.href = "../home/home.html";
 }
 
@@ -14,6 +23,7 @@ var requestOptions = {
 };
 
 const bodyTableHistory = document.getElementById("body-table-history");
+const bodyTableHistoryEdoeit = document.getElementById("body-table-history-edoeit");
 
 let index = 1;
 
@@ -21,31 +31,54 @@ fetch("https://harpay-api.herokuapp.com/transaksi/cekHistory", requestOptions)
   .then((response) => response.json())
   .then((result) => {
     result.data.forEach((history) => {
-      // console.log(history);
-
       // creating element
       const trEl = document.createElement("tr");
 
       const noTd = document.createElement("td");
-      const jenisTd = document.createElement("td");
-      const namaTd = document.createElement("td");
       const jenisTransaksiTd = document.createElement("td");
       const nominalPengeluaranTd = document.createElement("td");
       const jumlahTopupTd = document.createElement("td");
 
       noTd.innerText = index;
-      jenisTd.innerText = "HarPay";
-      namaTd.innerText = history.userNama;
       jenisTransaksiTd.innerText = history.jenisTransaksi;
       nominalPengeluaranTd.innerText = history.nominalPengeluaran;
       jumlahTopupTd.innerText = history.jumlahTopup;
 
       index++;
 
-      trEl.append(noTd, jenisTd, namaTd, jenisTransaksiTd, nominalPengeluaranTd, jumlahTopupTd);
+      trEl.append(noTd, jenisTransaksiTd, nominalPengeluaranTd, jumlahTopupTd);
       bodyTableHistory.append(trEl);
     });
 
-    //  to do for another e-money
+    // e-doeit
+    index = 1;
+
+    // setup new headers
+    var myHeadersEdoeit = new Headers();
+    myHeadersEdoeit.append("Authorization", `Bearer ${edoeit.jwt}`);
+    requestOptions.headers = myHeadersEdoeit;
+
+    fetch("https://e-doeit.herokuapp.com/api/history", requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        result.forEach((item) => {
+          // creating element
+          const trEl = document.createElement("tr");
+
+          const noTd = document.createElement("td");
+          const keteranganTd = document.createElement("td");
+          const jumlahTd = document.createElement("td");
+
+          noTd.innerText = index;
+          keteranganTd.innerText = item.keterangan;
+          jumlahTd.innerText = item.jumlah;
+
+          index++;
+
+          trEl.append(noTd, keteranganTd, jumlahTd);
+          bodyTableHistoryEdoeit.append(trEl);
+        });
+      })
+      .catch((error) => console.log("error", error));
   })
   .catch((error) => console.log("error", error));
